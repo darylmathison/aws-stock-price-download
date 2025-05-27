7691fb03ac8e19a2f37fce1993ab584504a01707
darylmathison <daryl.mathison@gmail.com>
2025-05-27 14:43:11 -0400

    Enhanced configuration.

--- src/main/java/com/darylmathison/market/SpringConfig.java
+++ src/main/java/com/darylmathison/market/SpringConfig.java
@@ -1,10 +1,14 @@
 package com.darylmathison.market;
 
 import net.jacobpeterson.alpaca.AlpacaAPI;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 @ComponentScan(basePackages = "com.darylmathison.market") // Scans components in this package
@@ -16,8 +20,20 @@ public class SpringConfig {
   @Value("${alpaca.secret}")
   private String alpacaSecretKey;
 
+  @Value("${aws.region}")
+  private String awsRegion;
+
   @Bean
   public AlpacaAPI alpacaAPI() {
     return new AlpacaAPI(alpacaApiKey, alpacaSecretKey);
+  }
+
+  @Bean
+  @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+  public S3Client s3Client() {
+    return S3Client
+        .builder()
+        .region(Region.of(awsRegion))
+        .build();
   }
 }
